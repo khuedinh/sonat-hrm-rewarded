a39c7591c85bb02715471b7c303a8c7520559f08
sonat-maivl <maivl@sonat.vn>
2024-08-19 18:32:53 +0700

    feat: recognition history

--- lib/src/models/recognition.dart
+++ lib/src/models/recognition.dart
@@ -1,21 +1,64 @@
 class Recognition {
   final String id;
   final String name;
-  final String? role;
   final String content;
   final DateTime createdAt;
-  final bool? hasRead;
-  final int quantity;
+  final int amount;
+  final String? recognitionValueId;
+  final String? message;
+  final String? imageUrl;
+  final String? cardContent;
+  final String? type;
 
   const Recognition({
     required this.id,
     required this.name,
     required this.content,
     required this.createdAt,
-    this.hasRead = false,
-    this.role = "",
-    this.quantity = 0,
+    required this.amount,
+    required this.type,
+    this.recognitionValueId,
+    this.message,
+    this.imageUrl,
+    this.cardContent,
+    
   });
+
+  factory Recognition.fromJson(Map<String, dynamic> json) {
+    return Recognition(
+      id: json['id'],
+      name: json['name'],
+      amount: json['amount'],
+      recognitionValueId: json['recognitionValueId'],
+      message: json['message'],
+      imageUrl: json['imageUrl'],
+      cardContent: json['cardContent'],
+      content: json['content'],
+      type: json['type'],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
+class RecognitionHistory {
+  final List<Recognition> sent;
+  final List<Recognition> received;
+
+  RecognitionHistory({
+    required this.sent,
+    required this.received,
+  });
+
+  factory RecognitionHistory.fromJson(Map<String, dynamic> json) {
+    print('json: $json');
+    return RecognitionHistory(
+      sent: (json['sent'] as List<Recognition>)
+          .map((item) => Recognition.fromJson(item as Map<String, dynamic>))
+          .toList(),
+      received: (json['received'] as List<Recognition>)
+          .map((item) => Recognition.fromJson(item as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 }
 
 class RecognitionValue {
--- lib/src/screens/tabs/recognition/recognition_screen.dart
+++ lib/src/screens/tabs/recognition/recognition_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sonat_hrm_rewarded/src/mock_data/recognition.dart';
+import 'package:sonat_hrm_rewarded/src/models/recognition.dart';
+import 'package:sonat_hrm_rewarded/src/service/api/recognition_api.dart';
 import 'package:sonat_hrm_rewarded/src/widgets/recognition/filters/recognition_filters.dart';
 import 'package:sonat_hrm_rewarded/src/widgets/recognition/p2p/p2p_tab.dart';
 import 'package:sonat_hrm_rewarded/src/widgets/recognition/team/team_tab.dart';
@@ -14,6 +16,30 @@ class RecognitionScreen extends StatefulWidget {
 }
 
 class _RecognitionScreenState extends State<RecognitionScreen> {
+  List<Recognition> sentNotifications = [];
+  List<Recognition> receivedNotifications = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications();
+  }
+
+  Future<void> fetchNotifications() async {
+    final response = await RecognitionApi.getHistory();
+
+    setState(() {
+      sentNotifications = (response['sent'] as List)
+          .map((item) => Recognition.fromJson(item as Map<String, dynamic>))
+          .toList();
+      receivedNotifications = (response['received'] as List)
+          .map((item) => Recognition.fromJson(item as Map<String, dynamic>))
+          .toList();
+      isLoading = false;
+      print("isLoading: $isLoading");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +60,9 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
 
     return DefaultTabController(
       initialIndex: 0,
-      length: 3,
+      length: 2,
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: 16.0, vertical: 8.0), // Adjust the padding as needed
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         child: Column(
           children: [
             Row(
@@ -47,9 +72,7 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const P2pTab()), // Replace NewPage with the actual page class
+                        MaterialPageRoute(builder: (context) => const P2pTab()),
                       );
                     },
                     child: const Column(
@@ -70,8 +93,7 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                const TeamTab()), // Replace NewPage with the actual page class
+                            builder: (context) => const TeamTab()),
                       );
                     },
                     child: const Column(
@@ -98,56 +120,70 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
               ],
             ),
             const SizedBox(height: 8.0),
-            const Row(
+            Row(
               children: [
                 Expanded(
                   child: Card(
                     child: Padding(
-                      padding: EdgeInsets.all(
-                          8.0), // Optional: Add padding inside the card
+                      padding: const EdgeInsets.all(8.0),
                       child: Row(
                         children: [
-                          Icon(
+                          const Icon(
                             Icons.output_outlined,
                             color: Colors.red,
                           ),
-                          SizedBox(
-                              width:
-                                  8.0), // Optional: Add spacing between icon and text
+                          const SizedBox(width: 8.0),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("Total Sent"),
-                              Text("2000 Points"),
+                              const Text("Total Sent"),
+                              isLoading
+                                  ? const SizedBox(
+                                      width: 14.0,
+                                      height: 14.0,
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : sentNotifications.isEmpty
+                                      ? const Text("0 Points")
+                                      : Text(
+                                          "${sentNotifications.fold(0, (previousValue, element) => previousValue + element.amount)} Points",
+                                        ),
                             ],
-                          ),
+                          )
                         ],
                       ),
                     ),
                   ),
                 ),
-                SizedBox(width: 8.0),
+                const SizedBox(width: 8.0),
                 Expanded(
                   child: Card(
                     child: Padding(
-                      padding: EdgeInsets.all(
-                          8.0), // Optional: Add padding inside the card
+                      padding: const EdgeInsets.all(8.0),
                       child: Row(
                         children: [
-                          Icon(
+                          const Icon(
                             Icons.input_outlined,
                             color: Colors.green,
                           ),
-                          SizedBox(
-                              width:
-                                  8.0), // Optional: Add spacing between icon and text
+                          const SizedBox(width: 8.0),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("Total Received"),
-                              Text("1600 Points"),
+                              const Text("Total Received"),
+                              isLoading
+                                  ? const SizedBox(
+                                      width: 14.0,
+                                      height: 14.0,
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : receivedNotifications.isEmpty
+                                      ? const Text("0 Points")
+                                      : Text(
+                                          "${receivedNotifications.fold(0, (previousValue, element) => previousValue + element.amount)} Points",
+                                        ),
                             ],
-                          ),
+                          )
                         ],
                       ),
                     ),
@@ -163,7 +199,7 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                   "History",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 20.0, // Adjust the font size as needed
+                    fontSize: 20.0,
                   ),
                 ),
                 IconButton(
@@ -176,75 +212,121 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
               indicatorColor: theme.colorScheme.primary,
               labelPadding: const EdgeInsets.symmetric(horizontal: 16),
               tabs: const [
-                Tab(text: "All"),
                 Tab(text: "Sent"),
                 Tab(text: "Received"),
               ],
             ),
             const SizedBox(height: 8.0),
             Expanded(
-              child: TabBarView(
-                children: [
-                  ListView.builder(
-                    itemCount: historyList.length,
-                    itemBuilder: (context, index) {
-                      final notification = historyList[index];
-                      return ListTile(
-                        titleAlignment: ListTileTitleAlignment.center,
-                        leading: const SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: CircleAvatar(
-                            child: Icon(Icons.person),
-                          ),
-                        ),
-                        title: Text(
-                          notification.name,
-                          style: theme.textTheme.titleMedium!.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.start,
-                        ),
-                        subtitle: notification.role == null ||
-                                notification.role!.isEmpty
-                            ? null
-                            : Text(notification.role!),
-                        trailing: Text(
-                          notification.quantity > 0
-                              ? '+${notification.quantity.toString()}'
-                              : notification.quantity.toString(),
-                          style: TextStyle(
-                            color: notification.quantity > 0
-                                ? Colors.green
-                                : Colors.red,
-                          ),
-                        ),
-                        onTap: () {},
-                      );
-                    },
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.folder_off),
-                        onPressed: () {},
-                      ),
-                      const Text("No data found."),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.folder_off),
-                        onPressed: () {},
-                      ),
-                      const Text("No data found."),
-                    ],
-                  ),
-                ],
-              ),
+              child: isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : TabBarView(
+                      children: [
+                        sentNotifications.isEmpty
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.folder_off),
+                                    onPressed: () {},
+                                  ),
+                                  const Text("No data found."),
+                                ],
+                              )
+                            : ListView.builder(
+                                itemCount: sentNotifications.length,
+                                itemBuilder: (context, index) {
+                                  final notification = sentNotifications[index];
+                                  return ListTile(
+                                    titleAlignment:
+                                        ListTileTitleAlignment.center,
+                                    leading: const SizedBox(
+                                      width: 50,
+                                      height: 50,
+                                      child: CircleAvatar(
+                                        child: Icon(Icons.person),
+                                      ),
+                                    ),
+                                    title: Text(
+                                      notification.name,
+                                      style:
+                                          theme.textTheme.titleMedium!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    // subtitle: notification.role == null ||
+                                    //         notification.role!.isEmpty
+                                    //     ? null
+                                    //     : Text(notification.role!),
+                                    trailing: Text(
+                                      notification.amount > 0
+                                          ? '+${notification.amount.toString()}'
+                                          : notification.amount.toString(),
+                                      style: TextStyle(
+                                        color: notification.amount > 0
+                                            ? Colors.green
+                                            : Colors.red,
+                                      ),
+                                    ),
+                                    onTap: () {},
+                                  );
+                                },
+                              ),
+                        receivedNotifications.isEmpty
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.folder_off),
+                                    onPressed: () {},
+                                  ),
+                                  const Text("No data found."),
+                                ],
+                              )
+                            : ListView.builder(
+                                itemCount: receivedNotifications.length,
+                                itemBuilder: (context, index) {
+                                  final notification =
+                                      receivedNotifications[index];
+                                  return ListTile(
+                                    titleAlignment:
+                                        ListTileTitleAlignment.center,
+                                    leading: const SizedBox(
+                                      width: 50,
+                                      height: 50,
+                                      child: CircleAvatar(
+                                        child: Icon(Icons.person),
+                                      ),
+                                    ),
+                                    title: Text(
+                                      notification.name,
+                                      style:
+                                          theme.textTheme.titleMedium!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    // subtitle: notification.role == null ||
+                                    //         notification.role!.isEmpty
+                                    //     ? null
+                                    //     : Text(notification.role!),
+                                    trailing: Text(
+                                      notification.amount > 0
+                                          ? '+${notification.amount.toString()}'
+                                          : notification.amount.toString(),
+                                      style: TextStyle(
+                                        color: notification.amount > 0
+                                            ? Colors.green
+                                            : Colors.red,
+                                      ),
+                                    ),
+                                    onTap: () {},
+                                  );
+                                },
+                              ),
+                      ],
+                    ),
             ),
           ],
         ),
